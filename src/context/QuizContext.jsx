ad4f955ee598b46fcb0e5823d3f2bc75888de2d5
Patrickodida <patrick.o.odida@gmail.com>
2025-07-08 12:25:55 +0300

    Add quiz context logic

--- src/context/QuizContext.jsx
+++ src/context/QuizContext.jsx
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { Children, createContext, useContext, useState } from "react";
+import { questions } from '../data/questions';
 
-const QuizContext = () => {
+const QuizContext = createContext();
+
+export const QuizProvider = ({ Children }) => {
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [score, setScore] = useState(0);
+    const [quizFinished, setQuizFinished] = useState(false);
+
+    const nextQuestion = (isCorrect) => {
+        if (isCorrect) setScore(prev => prev + 1);
+        if(currentQuestion + 1 < questions.length) {
+            setCurrentQuestion(prev => prev + 1);
+        } else {
+            setQuizFinished(true);
+        }
+    };
+
+    const resetQuiz = () => {
+        setScore(0);
+        setCurrentQuestion(0);
+        setQuizFinished(false);
+    };
+
     return (
-        <div>
-
-        </div>
+        <QuizContext.Provider
+        value={{ questions, currentQuestion, score, quizFinished, nextQuestion, resetQuiz }}>
+            { Children }
+        </QuizContext.Provider>
     )
 }
-export default QuizContext;
+export const useQuiz = () => useContext(QuizContext);
