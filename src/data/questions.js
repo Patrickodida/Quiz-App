05b1287f11659e87422eb7b03d9c5d10f028fbba
Patrickodida <patrick.o.odida@gmail.com>
2025-07-08 11:58:36 +0300

    Add Project Structure: Components, context, data, and different pages

---
+++ src/data/questions.js
@@ -0,0 +1,11 @@
+export const questions = [
+    {question: "What is the capital of France?",
+    options: ["Paris", "Berlin", "Rome", "Madrid"],
+    answer: "Paris"
+    },
+    {
+    question: "Which planet is known as the Red Planet?",
+    options: ["Earth", "Mars", "Jupiter", "Saturn"],
+    answer: "Mars"
+    }
+]
