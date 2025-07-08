02534820ac02dc5c063cdd5689386f4b269e4f34
Patrickodida <patrick.o.odida@gmail.com>
2025-07-08 12:10:35 +0300

    Setup Routes for different pages; home, quiz, and result

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Quiz from "./pages/Quiz";
+import Result from "./pages/Result";
+import { QuizProvider }  from "./context/QuizContext";
 
 function App(){
   return (
-    <div>
-      <h1>Quiz App</h1>
-    </div>
+    <QuizProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/quiz" element={<Quiz />} />
+          <Route path="/result" element={<Result />} />
+        </Routes>
+      </BrowserRouter>
+    </QuizProvider>
+
   )
 }
 export default App
